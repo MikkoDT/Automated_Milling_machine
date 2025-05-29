fe0a89418eec322dff0c632e4342360504b960d1
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2025-05-29 12:23:06 +0800

    Update linear_actuator_test.ino

--- linear_actuator_test/linear_actuator_test.ino
+++ linear_actuator_test/linear_actuator_test.ino
@@ -1,6 +1,6 @@
 // Relay Signal Pins
-const int forwardPin2 = 32; 
-const int reversePin2 = 30;
+const int forwardPin2 = 30; 
+const int reversePin2 = 32;
 
 // Relay Module Power Control
 const int relayPower = 24;
@@ -12,27 +12,20 @@ void setup() {
 
   // Turn ON relay module
   digitalWrite(relayPower, HIGH);
-  //delay(500); // wait for relay module to power up
+  
 
   // Make sure all directions are OFF first
   digitalWrite(forwardPin2, LOW);
   digitalWrite(reversePin2, LOW);
 
-  //delay(500); // Optional
-
-  // Activate forward
- 
-  //digitalWrite(forwardPin2, HIGH);
-  //delay(700);
 
   digitalWrite(forwardPin2, HIGH);
-  delay(5000); // Run time
+  delay(10000); // Run time
 
   // Stop actuator
   digitalWrite(forwardPin2, LOW);
   digitalWrite(reversePin2, LOW);
 
-  //delay(500); // Optional delay before full shutdown
 
   // Turn OFF relay module power
   digitalWrite(relayPower, LOW);
